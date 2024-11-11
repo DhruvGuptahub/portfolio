723d2e9e7acd95ce917a236fe3926d2bfdda4510
Dhruv gupta <145158632+dhruv8815@users.noreply.github.com>
2024-11-11 15:09:45 +0530

    Add downloadresume

--- src/component/HeroImg.jsx
+++ src/component/HeroImg.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './HeroImgStyles.css'
 import IntroImg from '../assets/intro-bg.jpg'
 import { Link } from 'react-router-dom'
+import Typed from 'typed.js'
+import pdf from '../pdf/DHRUVGUPTA_RESUME.pdf'
 
 function HeroImg() {
+    const typedRef = useRef(null)
+    const typedRef2 = useRef(null)
+
+    useEffect(() => {
+        const options = {
+            strings: ["HI, I'M DHRUV GUPTA."],
+            typeSpeed: 50,
+            backSpeed: 50,
+            loop: true
+        }
+
+        const typed = new Typed(typedRef.current, options)
+
+        return () => {
+            typed.destroy()
+        }
+    }, [])
+
     return (
         <div className='hero'>
             <div className="mask">
                 <img className='into-img' src={IntroImg} alt="Introimg" />
             </div>
             <div className='content'>
-                <p>HI, I'M DHRUV GUPTA.</p>
-                <h1>React Developer</h1>
+                <p ref={typedRef}></p>
+                <h1>React Developer.</h1>
                 <div>
                     <Link to='/project' className='btn'>Projects</Link>
                     <Link to='/contact' className='btn btn-light'>Contact</Link>
+                </div>
+                <div className='btn btn-light resume'>
+                    <a href={pdf} download="DhruvResume.pdf">Download Resume</a>
                 </div>
             </div>
         </div>
