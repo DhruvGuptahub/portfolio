f05ff6109438779cdd3313e31f785a603f397507
Dhruv gupta <145158632+dhruv8815@users.noreply.github.com>
2024-11-11 16:07:34 +0530

    add social links

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,4 +1,6 @@
-import { FaFacebook, FaGithub, FaHome, FaLinkedin, FaMailBulk, FaMailchimp, FaPhone, FaTwitter } from "react-icons/fa"
+import { FaFacebook, FaGithub, FaHome, FaLinkedin, FaMailBulk, FaMailchimp, FaPhone } from "react-icons/fa"
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { SiGmail } from "react-icons/si";
 import "./FooterStyles.css"
 
 import React from 'react'
@@ -31,12 +33,29 @@ function Footer() {
                 <div className="right">
                     <h4>About Me</h4>
                     <p>This is me Dhruv Gupta. I enjoy discussing new projects and design challenges. I love to create dynamic UI's. </p>
-                    <div className="social">
-                        <FaFacebook size={30} style={{ color: "#fff", marginRight: "1rem" }} />
-                        <FaTwitter size={30} style={{ color: "#fff", marginRight: "1rem" }} />
-                        <FaLinkedin size={30} style={{ color: "#fff", marginRight: "1rem" }} />
-                        <FaGithub size={30} style={{ color: "#fff", marginRight: "1rem" }} />
+
+                    <div className="contact-icon">
+                        <a href='https://www.facebook.com/profile.php?id=100009513211009' target='_blank' className="items">
+                            <FaFacebook className='icons' />
+                        </a>
+                        <a href='https://www.linkedin.com/in/dhruv-gupta-85a0851aa?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' target='_blank' className="items">
+                            <FaLinkedin className='icons' />
+                        </a>
+                        <a href='https://x.com/dhruvgupta8815' target='_blank' className="items">
+                            <FaSquareXTwitter className='icons' />
+                        </a>
+                        <a href='https://github.com/DhruvGuptahub' target='_blank' className="items">
+                            <FaGithub className='icons' />
+                        </a>
+                        <a href='mailto:dhruv8815@gmail.com' target='_blank' className="items">
+                            <SiGmail className='icons' />
+                        </a>
                     </div>
+
+                    {/* <FaFacebook size={30} style={{ color: "#fff", marginRight: "1rem" }} />
+                        <FaTwitter <a href="https://x.com/dhruvgupta8815" size={30} style={{ color: "#fff", marginRight: "1rem" }} ></a>/>
+                        <FaLinkedin size={30} style={{ color: "#fff", marginRight: "1rem" }} />
+                        <FaGithub size={30} style={{ color: "#fff", marginRight: "1rem" }} /> */}
                 </div>
             </div>
         </div>
